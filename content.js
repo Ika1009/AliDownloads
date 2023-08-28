94c612994fe346cb45b96a65abdd9196a73e2d44
Ika1009 <ilija.doncic@yahoo.com>
2023-08-28 17:07:22 +0200

    separeted logic for aliexpres and alibaba

--- content.js
+++ content.js
@@ -2,21 +2,37 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message.action === 'startDownload') {
         const downloadOptions = message.downloadOptions;
 
-        // Implement your extraction logic based on downloadOptions
-        // For example:
-        if (downloadOptions.images) {
-            // Extract images and save to the path
-        }
-
-        if (downloadOptions.videos) {
-            // Extract videos and save to the path
-        }
-
-        if (downloadOptions.description) {
-            // Extract product description and save to the path
+        if (window.location.href.includes("aliexpress.com")) {
+            extractFromAliExpress(downloadOptions);
+        } else if (window.location.href.includes("alibaba.com")) {
+            extractFromAlibaba(downloadOptions);
         }
 
         // Send back a message if needed
         sendResponse({status: "downloaded"});
     }
 });
+
+function extractFromAliExpress(options) {
+    if (options.images) {
+        // AliExpress specific logic to extract images
+    }
+    if (options.videos) {
+        // AliExpress specific logic to extract videos
+    }
+    if (options.description) {
+        // AliExpress specific logic to extract product description
+    }
+}
+
+function extractFromAlibaba(options) {
+    if (options.images) {
+        // Alibaba specific logic to extract images
+    }
+    if (options.videos) {
+        // Alibaba specific logic to extract videos
+    }
+    if (options.description) {
+        // Alibaba specific logic to extract product description
+    }
+}
