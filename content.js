e3af0443e392e2ac242d3ce1392038455c198d2c
Ika1009 <ilija.doncic@yahoo.com>
2023-09-02 17:23:17 +0200

    Added extraction of img and video urls, not tested

--- content.js
+++ content.js
@@ -18,8 +18,9 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     }
 });
 
-// ... rest of your content.js code ...
-
+function delay(ms) {
+    return new Promise(res => setTimeout(res, ms));
+}
 
 function extractFromAliExpress(options) {
     console.log("ALIEKSPRES");
@@ -35,16 +36,52 @@ function extractFromAliExpress(options) {
     }
 }
 
-function extractFromAlibaba(options) {
-    console.log("ALIBABA");
-    console.log(options);
-    if (options.images) {
-        // Alibaba specific logic to extract images
+async function extractFromAlibaba(options) {
+
+    // Click on the "View larger image" button to open the image/video modal
+    if (options.images || options.videos) {
+        const potentialButtons = Array.from(document.querySelectorAll('span[data-spm-anchor-id^="a2700.details"]'));
+        const viewLargerImageBtn = potentialButtons.find(btn => btn.innerText.trim() === "View larger image");
+
+        if (viewLargerImageBtn) {
+            viewLargerImageBtn.click();
+            await delay(5000); // Wait for 5 seconds for modal to load fully
+
+            // After the modal loads, extract the image and video URLs
+            const sliderDiv = document.querySelector('div.slider-list[data-spm-anchor-id^="a2700.details.0."]');
+            if (sliderDiv && options.images) {
+                const sliderItems = sliderDiv.children;
+                const imgURLs = [];
+            
+                for (let item of sliderItems) {
+                    const imgElement = item.querySelector('img');
+                    if (imgElement) {
+                        let highResURL = imgElement.src.replace('250x250', '960x960');
+                        imgURLs.push(highResURL);
+                    }
+                }
+            
+                console.log("Extracted high-res image URLs:", imgURLs);
+            
+                // TODO: Add logic to extract video URLs if present and save everything to a .zip file
+            }
+        }
     }
+
+    // If the options.videos is true, fetch the video URLs
     if (options.videos) {
-        // Alibaba specific logic to extract videos
+        const videoElement = document.querySelector('video'); // Searching in entire document since the video might be outside the clicked item
+        if (videoElement) {
+            let videoSrc = videoElement.src;
+            videoURLs.push(videoSrc);
+        }
     }
+
     if (options.description) {
-        // Alibaba specific logic to extract product description
+        // Extract product description
+        const descriptionElem = document.querySelector('.product-description-section');
+        const description = descriptionElem ? descriptionElem.innerText : "";
+        console.log(description);
+        // Download or further processing logic for description
     }
 }
