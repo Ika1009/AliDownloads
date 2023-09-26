0031404004e35e5637bb6df6c4b68a2a845b10c7
Ika1009 <ilija.doncic@yahoo.com>
2023-09-26 16:21:14 +0200

    uploaded last website

---
+++ website/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "index2.html"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-black': '#242121',
+        'custom-purple': '#5065A8',
+      },
+    },
+  },
+  plugins: [],
+
+}
+
